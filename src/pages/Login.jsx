3b107adbdee81bd6123b2f850497e8bb216eb8ed
Steven Gomez <brastegocha@gmail.com>
2025-12-03 23:10:53 -0500

    reversing the redirect change

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -20,7 +20,7 @@ export default function Login() {
 
     if (res.success) {
       alert("Logged in!");
-     Navigate('/dashboard');
+     Navigate('/');
     } else {
       setMessage(res.message || "Error logging in");
     }
